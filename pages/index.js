59bb2380f35d0705b0dc0de6a3e9093061dbd28f
Kenneth Campos Campos <kcamposc@ice.go.cr>
2020-12-01 16:04:49 -0600

    Setting up CI/CD

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to I'am Kenneth Campos
+          I'am Kenneth Campos
         </h1>
 
         <p className={styles.description}>
